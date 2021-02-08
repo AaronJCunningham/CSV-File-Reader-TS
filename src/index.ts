131e0add4863588fa4e2436ab8a58a94914264f5
Aaron J Cunningham <aaronjosephcunningham@gmail.com>
2021-02-08 14:23:40 +0100

    2nd refractor

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,8 @@
-import { MatchResult } from "./MatchResult";
-import { CsvFileReader } from "./CsvFileReader";
 import { MatchReader } from "./MatchReader";
+import { Summary } from "./Summary";
 
-const csvFileReader = new CsvFileReader("football.csv");
+const matchReader = MatchReader.fromCsv("football.csv");
+const summary = Summary.winsAnalysisWithHtmlReport("Man United");
 
-const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
-
-let manWins = 0;
-
-for (let match of matchReader.matches) {
-  if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
-    manWins++;
-  } else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
-    manWins++;
-  }
-}
-
-console.log(manWins);
+summary.buildAndPrintReport(matchReader.matches);
